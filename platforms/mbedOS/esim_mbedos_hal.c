ce8f04a9332f574348242c0a59572281b0ffa165
viktor.toth <viktor.toth@r-das.sk>
2021-06-23 09:53:12 +0200

    Added mbed os hal

---
+++ platforms/mbedOS/esim_mbedos_hal.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "esim_mbedos_hal.h"
+
+#include "mbed.h"
+
+void* platform_malloc(size_t size){
+    return malloc(size);
+}
+
+void platform_free(void* pointer){
+    free(pointer);
+}
+
+void platform_log_info(char* format, ...){
+    #ifdef ESIM_LOGGING
+    va_list args;
+
+    printf("ESIM INFO: ");
+
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+
+    printf("\r\n");
+    #endif
+}
+
+void platform_log_error(char* format, ...){
+    #ifdef ESIM_LOGGING
+    va_list args;
+
+    printf("ESIM ERROR: ");
+    
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+
+    printf("\r\n");
+    #endif
+}
