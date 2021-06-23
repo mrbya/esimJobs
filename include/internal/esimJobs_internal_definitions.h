6b9566dd8b0d27b2c786cfd85f20a5cb827d20d2
viktor.toth <viktor.toth@r-das.sk>
2021-06-23 10:18:44 +0200

    Added missing files

---
+++ include/internal/esimJobs_internal_definitions.h
@@ -0,0 +1,17 @@
+#ifndef ESIMJOBS_INTERNAL_DEFINITIONS_H_
+#define ESIMJOBS_INTERNAL_DEFINITIONS_H_
+
+#include "esimJobs_definitions.h"
+
+#define ESIM_RECIPE_ID_KEY  "RecipeId"
+#define ESIM_CYCLES_CNT_KEY "CyclesCount"
+#define ESIM_INFINITE_KEY   "IsInfinite"
+#define ESIM_SCRIPT_KEY     "script"
+#define ESIM_CMD_KEY        "EsimCommandKey"
+#define ESIM_ARG_KEY        "Values"
+#define ESIM_ARG_TYPE_KEY   "ArgumentType"
+#define ESIM_ARG_VAL_KEY    "Value"
+
+
+
+#endif//ESIMJOBS_INTERNAL_DEFINITIONS_H_
